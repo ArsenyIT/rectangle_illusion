99b0fc2985f8e6f9123953805c6e0305377b27ef
ArsenyIT <st.2.udalov@gmail.com>
2025-03-01 00:12:15 +0300

    д/з

---
+++ all_colors.py
@@ -0,0 +1,67 @@
+import pygame
+
+Salmon = (250, 128, 114) #
+DarkSalmon = (233, 150, 122) #
+LightSalmon = (255, 160, 122) #
+Red = (255, 0, 0) #
+Pink = (255, 192, 203) #
+Coral = (255, 127, 80) #
+Tomato = (255, 99, 71) #
+Orange = (255, 165, 0) #
+Gold = (255, 215, 0) #
+Yellow = (255, 255, 0) #
+Khaki = (240, 230, 140) #
+Plum = (221, 160, 221) #
+Violet = (238, 130, 238) #
+Magenta = (255, 0, 255) #
+Purple = (128, 0, 128) #
+Indigo = (75, 0, 130) #
+Wheat = (245, 222, 179) #
+Tan = (210, 180, 140) #
+RosyBrown = (188, 143, 143) #
+SandyBrown = (244, 164, 96) #
+Peru = (205, 133, 63) #
+Chocolate = (210, 105, 30) #
+SaddleBrown = (139, 69, 19) #
+Sienna = (160, 82, 45) #
+Brown = (165, 42, 42) #
+Maroon = (128, 0, 0) #
+GreenYellow = (173, 255, 47) #
+Green = (0, 255, 0) #
+LimeGreen = (50, 205, 50) #
+SeaGreen = (46, 139, 87) #
+Lime = (0, 128, 0) #
+Olive = (128, 128, 0) #
+DarkCyan = (0, 139, 139) #
+Teal = (0, 128, 128) #
+Aqua = (0, 255, 255) #
+Cyan = (0, 255, 255) #
+LightCyan = (224, 255, 255) #
+Blue = (0, 0, 255) #
+Navy = (0, 0, 128) #
+White = (255, 255, 255) #
+Snow = (255, 250, 250) #
+Azure = (240, 255, 255) #
+Beige = (245, 245, 220) #
+Ivory = (255, 255, 240) #
+Linen = (250, 240, 230) #
+LavenderBlush = (255, 240, 245) #
+MistyRose = (255, 228, 225) #
+Gainsboro = (220, 220, 220) #
+LightGrey = (211, 211, 211) #
+Silver = (192, 192, 192) #
+DarkGrey = (169, 169, 169) #
+Grey = (128, 128, 128) #
+DimGrey = (105, 105, 105) #
+LightSlateGrey = (119, 136, 153) #
+SlateGrey = (112, 128, 144) #
+DarkSlateGrey = (47, 79, 79) #
+Black = (0, 0, 0) #
+
+colors = [Black, White, DarkSlateGrey, SlateGrey, Red, Green, Blue, Orange, Yellow, Navy, Grey, GreenYellow,
+          Brown, RosyBrown, SandyBrown, SaddleBrown, Lime, LimeGreen, Tan, Peru, Cyan, LightCyan, DarkCyan,
+          Salmon, DarkSalmon, LightSalmon, Linen, Silver, DimGrey,LightGrey, DarkGrey, Ivory, Beige, Azure,
+          Snow, Aqua, Teal, Olive, SeaGreen, Sienna, Chocolate, Maroon, Wheat, Purple, Indigo, Violet, Plum,
+          Magenta, Pink, Gold, Coral, Tomato, Khaki, LightSlateGrey, Gainsboro, MistyRose, LavenderBlush]
+
+green = pygame.Color(0, 255, 0, 128)
